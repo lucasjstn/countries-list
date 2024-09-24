79b1d173bad7f42682ac42c9398979b73d8c89f2
Lucas Oliveira <lucas7justino@gmail.com>
2024-09-24 16:57:27 -0300

    feat: adicionado filtros e busca

--- src/components/Navbar/SearchInput.tsx
+++ src/components/Navbar/SearchInput.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import { CiSearch } from 'react-icons/ci';
+import { useAppContext } from '../../screens/Home/Home';
 const SearchInput = () => {
+    const { searchTerm, setSearchTerm, setPage } = useAppContext();
+    const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setSearchTerm(event.target.value);
+        setPage(1); // Reseta para página 1 quando busca
+    };
     return (
         <div className='w-[50vw] md:w-[35vw] max-w-[70vw]  '>
             <label className='input input-bordered flex items-center gap-2 '>
-                <input type='text' className='min-w-6' placeholder='Pesquisar' />
-                <CiSearch className='ml-auto min-w-6'/>
+                <input
+                    type='text'
+                    className='min-w-6'
+                    placeholder='Buscar país'
+                    onChange={handleSearch}
+                    value={searchTerm}
+                />
+                <CiSearch className='ml-auto min-w-6' />
             </label>
         </div>
     );
