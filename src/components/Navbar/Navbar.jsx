d44b90fd85d4d30d8f7bc67d98e52563499acaa4
Lucas Oliveira <lucas7justino@gmail.com>
2024-09-28 03:36:40 -0300

    adicionado componente de toggle dark mode

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -12,44 +12,46 @@ const themes = {
 };
 
 const Navbar = () => {
+    const [theme, setTheme] = useState(
+        sessionStorage.getItem('theme') || themes.retro 
+    );
+
     const [showElement, setShowElement] = useState(
         sessionStorage.getItem('showElement') === 'true'
     );
 
-    const [theme, setTheme] = useState(
-        sessionStorage.getItem('theme') 
-    )
-
     const handleTheme = () => {
         const newTheme = theme === themes.retro ? themes.coffee : themes.retro;
-        sessionStorage.setItem('theme', theme);
-        document.documentElement.setAttribute('data-theme', theme);
+        sessionStorage.setItem('theme', newTheme); 
         setTheme(newTheme);
     };
 
-    
-
     const toggleShowElement = () => {
         setShowElement(!showElement);
-        sessionStorage.setItem('showElement', !showElement);
+        sessionStorage.setItem('showElement', !showElement); 
     };
 
+    
     useEffect(() => {
         document.documentElement.setAttribute('data-theme', theme);
-    }, [theme])
+    }, [theme]);
 
     return (
         <nav className='flex w-full flex-col justify-center items-center gap-8 border rounded shadow-lg py-3  h-46 '>
             <div className=' md:items-center flex flex-row items-center justify-center h-full'>
                 <Link to={'/'}>
-                    <h1 className='uppercase text-4xl'>Países</h1>
+                    <h1 className='uppercase text-4xl'>Lista de Países</h1>
                 </Link>
             </div>
             <label className='swap swap-rotate'>
-                <input type='checkbox' onChange={handleTheme} />
-                {/* sun icon*/}
+                
+                <input
+                    type='checkbox'
+                    onChange={handleTheme}
+                    checked={theme === themes.coffee} 
+                />
+               
                 <BsSunFill className='swap-on h-4 w-4' />
-                {/* moon icon*/}
                 <BsMoonFill className='swap-off h-4 w-4' />
             </label>
             <div className='gap-4 flex flex-col md:flex-row'>
