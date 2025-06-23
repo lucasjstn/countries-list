3b1899cfe9432be25cab48132dcd2f0de499e32d
Lucas Oliveira <72674138+lucasjstn@users.noreply.github.com>
2025-06-23 11:44:14 -0300

    Update Home.jsx

--- src/screens/Home/Home.jsx
+++ src/screens/Home/Home.jsx
@@ -7,7 +7,7 @@ import { useParams } from 'react-router-dom';
 
 export const AppContext = createContext('');
 export const useAppContext = () => useContext(AppContext);
-const URL = 'https://restcountries.com/v3.1/all';
+const URL = 'https://restcountries.com/v3.1/independent?status=true';
 
 const Home = () => {
     const [page, setPage] = useState(
